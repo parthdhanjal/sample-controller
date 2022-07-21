d00c894484c763546f6db359bbbf06e4acc4c1d9
Parth Dhanjal <parthh.dhanjal@gmail.com>
2022-07-20 23:13:45 -0700

    New Update

--- handler/handler.go
+++ handler/handler.go
@@ -36,21 +36,11 @@ type SampleHandlerStructType struct {
 var log = log1.Log.WithName("controllers").WithName("SampleKind")
 
 func (sh *SampleHandlerStructType) SampleHandle(ctx context.Context, instance *cachev1alpha1.SampleKind) (ctrl.Result, error) {
-	// if !crInstance.ObjectMeta.DeletionTimestamp.IsZero() {
-	// 	return h.deletionReconciler(ctx, crInstance)
-	// }
-
-	// // Initialize resource
-	// log.Info("Initializing")
-	// if res, err := h.initialize(ctx, crInstance); err != nil {
-	// 	return res, err
-	// }
-
-	// The resource is being created or updated
-	return sh.createOrUpdateReconciler(ctx, instance)
+	// Create Or Delete Pods
+	return sh.createOrDeletePods(ctx, instance)
 }
 
-func (sh *SampleHandlerStructType) createOrUpdateReconciler(ctx context.Context, instance *cachev1alpha1.SampleKind) (ctrl.Result, error) {
+func (sh *SampleHandlerStructType) createOrDeletePods(ctx context.Context, instance *cachev1alpha1.SampleKind) (ctrl.Result, error) {
 	reqNumPods := int(instance.Spec.Size)
 	var podList *core.PodList
 	currentPods := len(podList.Items)
